20ac5bd3310b38c35e3e4d74fbf3852e55f4f32e
Eugene Moon <moonyoojin@gmail.com>
2020-09-24 12:30:32 -0400

    Clean up auth forms to move onto servers

--- db/migrate/20200921184521_create_users.rb
+++ db/migrate/20200921184521_create_users.rb
@@ -5,7 +5,7 @@ class CreateUsers < ActiveRecord::Migration[5.2]
       t.string :email, null: false
       t.string :session_token, null: false
       t.string :password_digest, null: false
-
+      
       t.timestamps
     end
 
