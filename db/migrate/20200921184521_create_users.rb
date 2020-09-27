e49aec79def8703fb62340f88f9eb822ba405152
Eugene Moon <moonyoojin@gmail.com>
2020-09-27 14:44:43 -0400

    Update users migration

--- db/migrate/20200921184521_create_users.rb
+++ db/migrate/20200921184521_create_users.rb
@@ -10,6 +10,7 @@ class CreateUsers < ActiveRecord::Migration[5.2]
     end
 
     add_index :users, :username, unique: true
+    add_index :users, :email, unique: true
     add_index :users, :session_token, unique: true
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -43,6 +43,7 @@ ActiveRecord::Schema.define(version: 2020_09_26_185311) do
     t.string "password_digest", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["session_token"], name: "index_users_on_session_token", unique: true
     t.index ["username"], name: "index_users_on_username", unique: true
   end
